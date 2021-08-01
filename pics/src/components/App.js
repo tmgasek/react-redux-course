0f37e0f5228ea1b01e2deb6a74818ffc44968fb5
tmgasek <tomaszgasek28@gmail.com>
2021-08-01 15:18:48 +0100

    user input done

---
+++ pics/src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+class App extends React.Component {
+  onSearchSubmit(term) {
+    console.log(term);
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ paddingTop: '10px' }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
+
+export default App;
