c08c468198b6ba2037fe05103c76bcfd8c88f457
tmgasek <tomaszgasek28@gmail.com>
2021-08-04 12:14:54 +0100

    useEffect timeout

---
+++ widgets/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+// import Accordion from './components/Accordion';
+import Search from './components/Search';
+
+// const items = [
+//   {
+//     title: 'question 1',
+//     content: 'content 1',
+//   },
+//   {
+//     title: 'question 2',
+//     content: 'content 2',
+//   },
+//   {
+//     title: 'question 3',
+//     content: 'content 3',
+//   },
+// ];
+
+const App = () => {
+  return (
+    <div>
+      {/* <Accordion items={items} /> */}
+      <Search />
+    </div>
+  );
+};
+
+export default App;
